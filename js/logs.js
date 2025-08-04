782a207720eadf7245e13a0257149ca00f7a06da
MaterialCycle <materialcycleall@gmail.com>
2025-08-04 15:51:37 +0200

    Update logs.js

--- js/logs.js
+++ js/logs.js
@@ -1,26 +1,9 @@
-// js/logs.js
-
-import { dbPromise } from './db.js';
-
-export async function addLog(log) {
-  const db = await dbPromise;
-  return db.put('logs', log);
-}
+import { addLog, getAllLogs } from './db.js';
 
 export async function fetchLogs() {
-  const db = await dbPromise;
-  return db.getAll('logs');
+  return getAllLogs();
 }
 
-export async function fetchLogsByTower(towerId) {
-  const db = await dbPromise;
-  const tx = db.transaction('logs');
-  const store = tx.objectStore('logs');
-  const allLogs = await store.getAll();
-  return allLogs.filter(log => log.towerId === towerId);
-}
-
-export async function deleteLog(id) {
-  const db = await dbPromise;
-  return db.delete('logs', id);
+export async function addLogRecord(log) {
+  return addLog(log);
 }
