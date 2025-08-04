5d8e01d1f8125c672bf095d513c24ae6182289ce
MaterialCycle <materialcycleall@gmail.com>
2025-08-04 15:15:26 +0200

    Update logs.js

--- js/logs.js
+++ js/logs.js
@@ -1,1 +1,10 @@
+// js/logs.js
+import { saveLog } from "./db.js";
+
+// Add a new log with unique id and timestamp
+export async function addLog(log) {
+  log.id = "log-" + Date.now();
+  log.timestamp = new Date().toISOString();
+  await saveLog(log);
+}
 
