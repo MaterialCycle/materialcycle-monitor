749cf9f181bd95c4c24892def80e8eff18d22306
MaterialCycle <materialcycleall@gmail.com>
2025-08-04 15:46:28 +0200

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,24 +1,43 @@
-// js/db.js
+import { openDB } from 'https://cdn.jsdelivr.net/npm/idb@7/+esm';
 
-import { openDB } from 'idb';
+const DB_NAME = 'MaterialCycleDB';
+const DB_VERSION = 1;
+const TOWER_STORE = 'towers';
+const LOG_STORE = 'logs';
 
-export const dbPromise = openDB('MaterialCycleDB', 1, {
+let dbPromise = openDB(DB_NAME, DB_VERSION, {
   upgrade(db) {
-    if (!db.objectStoreNames.contains('towers')) {
-      db.createObjectStore('towers', { keyPath: 'id' });
+    if (!db.objectStoreNames.contains(TOWER_STORE)) {
+      db.createObjectStore(TOWER_STORE, { keyPath: 'id' });
     }
-    if (!db.objectStoreNames.contains('logs')) {
-      db.createObjectStore('logs', { keyPath: 'id' });
+    if (!db.objectStoreNames.contains(LOG_STORE)) {
+      db.createObjectStore(LOG_STORE, { keyPath: 'id' });
     }
   }
 });
 
-export async function saveTower(tower) {
+export async function addTower(tower) {
   const db = await dbPromise;
-  return db.put('towers', tower);
+  return db.put(TOWER_STORE, tower);
 }
 
 export async function getAllTowers() {
   const db = await dbPromise;
-  return db.getAll('towers');
+  return db.getAll(TOWER_STORE);
+}
+
+export async function addLog(log) {
+  const db = await dbPromise;
+  return db.put(LOG_STORE, log);
+}
+
+export async function getAllLogs() {
+  const db = await dbPromise;
+  return db.getAll(LOG_STORE);
+}
+
+export async function clearAllData() {
+  const db = await dbPromise;
+  await db.clear(TOWER_STORE);
+  await db.clear(LOG_STORE);
 }
