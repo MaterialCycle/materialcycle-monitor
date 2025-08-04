a0d7fe8ce4086782b252b7f522d96cb4175f8642
MaterialCycle <materialcycleall@gmail.com>
2025-08-04 16:39:08 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,9 @@
+// js/utils.js
+
+// Hides all view sections (used when navigating between Towers, Logs, Dashboard)
+export function hideAllViews() {
+  document.querySelectorAll('.view').forEach(view => {
+    view.style.display = 'none';
+  });
+}
 
