c0a68f61401adc94ca013b913b0e93afa2245d35
MaterialCycle <materialcycleall@gmail.com>
2025-08-04 15:34:30 +0200

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,32 +1,24 @@
 // js/db.js
 
-// Initialize IndexedDB using idb library
-const dbPromise = idb.openDB("MaterialCycleDB", 1, {
+import { openDB } from 'idb';
+
+export const dbPromise = openDB('MaterialCycleDB', 1, {
   upgrade(db) {
-    if (!db.objectStoreNames.contains("towers")) {
-      db.createObjectStore("towers", { keyPath: "id" });
+    if (!db.objectStoreNames.contains('towers')) {
+      db.createObjectStore('towers', { keyPath: 'id' });
     }
-    if (!db.objectStoreNames.contains("logs")) {
-      db.createObjectStore("logs", { keyPath: "id" });
+    if (!db.objectStoreNames.contains('logs')) {
+      db.createObjectStore('logs', { keyPath: 'id' });
     }
   }
 });
 
-// Save or update a tower
 export async function saveTower(tower) {
   const db = await dbPromise;
-  await db.put("towers", tower);
+  return db.put('towers', tower);
 }
 
-// Get all towers
 export async function getAllTowers() {
   const db = await dbPromise;
-  return db.getAll("towers");
+  return db.getAll('towers');
 }
-
-// Save or update a daily log
-export async function saveLog(log) {
-  const db = await dbPromise;
-  await db.put("logs", log);
-}
-
