ed3e468aaf0dfcf5687db4844c8c57400ce05717
MaterialCycle <materialcycleall@gmail.com>
2025-08-04 15:15:01 +0200

    Update towers.js

--- js/towers.js
+++ js/towers.js
@@ -1,1 +1,15 @@
+// js/towers.js
+import { saveTower, getAllTowers } from "./db.js";
+
+// Add a new tower with unique id and timestamp
+export async function addTower(tower) {
+  tower.id = "tower-" + Date.now();
+  tower.createdAt = new Date().toISOString();
+  await saveTower(tower);
+}
+
+// Fetch all towers from IndexedDB
+export async function fetchTowers() {
+  return getAllTowers();
+}
 
